4a2045e9e94e4e9539a9b868053b1ff2c8eb85da
Mateusz <mateusz.wrzaszczak1997@gmail.com>
2021-03-30 17:12:29 +0200

    symfony & vue install

---
+++ assets/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>Test VUE.js</h2>
+        <button class="btn btn-primary" @click="counter++">Click me - {{ counter }}</button>
+        <div v-for="item in items" :key="item.id">
+            <h2>{{ item.title }}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                items: [
+                    {title: 'Zakupy', completed: false, id: 1},
+                    {title: 'Spacer', completed: false, id: 2},
+                    {title: 'Al', completed: false, id: 3},
+                ]
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style>
+
+</style>
